ae7adcd82cd9fcf6e6bcd0875dfac16427ab1bd3
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2021-08-17 13:11:11 -0300

    Add grid

--- turorial/money_model.py
+++ turorial/money_model.py
@@ -2,6 +2,7 @@
 
 from mesa import Agent, Model
 from mesa.time import RandomActivation
+from mesa.space import MultiGrid
 
 class MoneyAgent(Agent):
   """ An agent with fixed initial wealth."""
@@ -10,25 +11,39 @@ class MoneyAgent(Agent):
     self.wealth = 1
 
   def step(self):
-    # The agent's step will go here.
-    # For demonstration purposes we will print the agent's unique_id
+    # move into neighboring cell
+    possible_steps = self.model.grid.get_neighborhood(
+      self.pos,
+      moore=True,
+      include_center=False)
+    new_position = self.random.choice(possible_steps)
+    self.model.grid.move_agent(self, new_position)
 
-    if self.wealth == 0:
-      return
-    other_agent = self.random.choice(self.model.schedule.agents)
-    other_agent.wealth += 1
-    self.wealth -= 1
-        
+    # give money to roommate
+    if self.wealth > 0:
+      self.give_money_to_random_roommate()
+
+  def give_money_to_random_roommate(self):
+    roommates = self.model.grid.get_cell_list_contents([self.pos])
+    if len(roommates) > 1:
+      lucky = self.random.choice(roommates)
+      lucky.wealth += 1
+      self.wealth -= 1
 
 class MoneyModel(Model):
   """A model with some number of agents."""
-  def __init__(self, N):
+  def __init__(self, N, width, height):
     self.num_agents = N
+    self.grid = MultiGrid(width, height, True)
     self.schedule = RandomActivation(self)
+
     # Create agents
     for i in range(self.num_agents):
       a = MoneyAgent(i, self)
       self.schedule.add(a)
+      x = self.random.randrange(self.grid.width)
+      y = self.random.randrange(self.grid.height)
+      self.grid.place_agent(a, (x, y))
 
   def step(self):
     '''Advance the model by one step.'''
--- turorial/run_model.py
+++ turorial/run_model.py
@@ -1,6 +1,35 @@
 from money_model import MoneyModel
 
-model = MoneyModel(5)
+import matplotlib.pyplot as plt
+import numpy as np
 
-for i in range(10):
-  model.step()
+num_steps = 20
+num_models = 1
+num_agents = 50
+
+grid_width = 10
+grid_height = 10
+
+all_wealth = []
+#This runs the model 100 times, each model executing 10 steps.
+for j in range(num_models):
+  # Run the model
+  model = MoneyModel(num_agents, grid_width, grid_height)
+  for i in range(num_steps):
+    model.step()
+
+  # Store the results
+  for agent in model.schedule.agents:
+    all_wealth.append(agent.wealth)
+
+  agent_counts = np.zeros((model.grid.width, model.grid.height))
+  for cell in model.grid.coord_iter():
+      cell_content, x, y = cell
+      agent_count = len(cell_content)
+      agent_counts[x][y] = agent_count
+  plt.imshow(agent_counts, interpolation='nearest')
+  plt.colorbar()
+  plt.show()
+
+# plt.hist(all_wealth, bins=range(max(all_wealth)+1))
+# plt.show()
