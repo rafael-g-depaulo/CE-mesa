f06f65f1bcee198a033afca7dde4e49c9fbec64c
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2021-08-17 14:35:51 -0300

    Add Gini computation

--- turorial/money_model.py
+++ turorial/money_model.py
@@ -3,6 +3,14 @@
 from mesa import Agent, Model
 from mesa.time import RandomActivation
 from mesa.space import MultiGrid
+from mesa.datacollection import DataCollector
+
+def compute_gini(model):
+  agent_wealths = [agent.wealth for agent in model.schedule.agents]
+  x = sorted(agent_wealths)
+  N = model.num_agents
+  B = sum( xi * (N-i) for i,xi in enumerate(x) ) / (N*sum(x))
+  return (1 + (1/N) - 2*B)
 
 class MoneyAgent(Agent):
   """ An agent with fixed initial wealth."""
@@ -11,6 +19,12 @@ class MoneyAgent(Agent):
     self.wealth = 1
 
   def step(self):
+    # move and give money to roommate
+    self.move()
+    if self.wealth > 0:
+      self.give_money_to_random_roommate()
+
+  def move(self):
     # move into neighboring cell
     possible_steps = self.model.grid.get_neighborhood(
       self.pos,
@@ -19,10 +33,6 @@ class MoneyAgent(Agent):
     new_position = self.random.choice(possible_steps)
     self.model.grid.move_agent(self, new_position)
 
-    # give money to roommate
-    if self.wealth > 0:
-      self.give_money_to_random_roommate()
-
   def give_money_to_random_roommate(self):
     roommates = self.model.grid.get_cell_list_contents([self.pos])
     if len(roommates) > 1:
@@ -30,12 +40,12 @@ class MoneyAgent(Agent):
       lucky.wealth += 1
       self.wealth -= 1
 
-class MoneyModel(Model):
-  """A model with some number of agents."""
+class MoneyModel(Model): 
   def __init__(self, N, width, height):
     self.num_agents = N
     self.grid = MultiGrid(width, height, True)
     self.schedule = RandomActivation(self)
+    self.running = True
 
     # Create agents
     for i in range(self.num_agents):
@@ -45,6 +55,11 @@ class MoneyModel(Model):
       y = self.random.randrange(self.grid.height)
       self.grid.place_agent(a, (x, y))
 
+    # create data collector
+    self.datacollector = DataCollector(
+        model_reporters={"Gini": compute_gini},
+        agent_reporters={"Wealth": "wealth"})
+
   def step(self):
-    '''Advance the model by one step.'''
+    self.datacollector.collect(self)
     self.schedule.step()
